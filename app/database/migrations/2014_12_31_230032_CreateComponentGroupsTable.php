d00cd2dda516c4af522712d83ac93ab6dbcbdaad
James Brooks <jbrooksuk@me.com>
2015-01-01 16:27:47 +0000

    Import Schema

--- app/database/migrations/2014_12_31_230032_CreateComponentGroupsTable.php
+++ app/database/migrations/2014_12_31_230032_CreateComponentGroupsTable.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Schema;
 
 class CreateComponentGroupsTable extends Migration
 {
